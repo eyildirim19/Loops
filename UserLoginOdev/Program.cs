9b35dc6ebc998c82b744e569f6e027d00c515daf
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-13 09:42:36 +0300

    07.11.2021 tarihli ödevlerin yapımı....

---
+++ UserLoginOdev/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace UserLoginOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hak = 0;
+            do
+            {
+                Console.WriteLine("Kullanıcı Adını Giriniz");
+                string kadi = Console.ReadLine();
+
+                Console.WriteLine("Şifrenizi Giriniz");
+                string sifre = Console.ReadLine();
+
+                if (kadi.ToLower() == "admin" && sifre.ToLower() == "123")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{++hak}. kez hatalı giriş yaptınız. Kalan giriş hakkınız {3 - hak}");
+
+                }
+
+                if (hak == 3) // hak 3 ise
+                {
+                    Console.WriteLine("hatalı giriş yaptınız çıkıyorsunuz.....");
+                    Thread.Sleep(4000);// iki saniye bekle...
+                    //Console.ReadKey();
+                    return; // program metodundan çık...
+                }
+
+            } while (hak < 3);
+
+
+            Console.WriteLine("Hoş geldin admin.......");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VizeFinalOdev/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VizeFinalOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0;
+            Console.WriteLine("Öğrenci Sayısını Giriniz");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            float enkOrtalama = 100, enbOrtalama = 0, toplamOrtalama = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}. öğrenci vizesi");
+                float vize = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine($"{i + 1}. öğrenci finali");
+                float final = Convert.ToSingle(Console.ReadLine());
+
+                float ortalama = (vize * 0.4f) + (final * 0.6f);
+
+                Console.WriteLine($"Öğrenci Ortalaması = {ortalama}");
+
+
+                if (ortalama > enbOrtalama) // if bloğunda tek satır komutunuz varsa scope açmanıza gerekyok...
+                    enbOrtalama = ortalama;
+
+
+                if (ortalama < enkOrtalama)
+                    enkOrtalama = ortalama;
+
+                toplamOrtalama += ortalama;
+            }
+
+            toplamOrtalama = toplamOrtalama / n;
+
+            Console.WriteLine($"Sınıf Ortalaması = {toplamOrtalama}");
+            Console.WriteLine($"En büyük Öğrenci Ortalaması = {enbOrtalama}");
+            Console.WriteLine($"En küçük Öğrenci Ortalaması = {enkOrtalama}");
+
+            Console.ReadKey();
+        }
+    }
+}
